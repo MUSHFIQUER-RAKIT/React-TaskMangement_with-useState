c79bc54135221d922bddbcddb0e79edc9523f13d
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-10-28 12:00:02 +0600

    Implement Search

--- src/TaskManager/SearchTask.jsx
+++ src/TaskManager/SearchTask.jsx
@@ -1,4 +1,13 @@
-export default function SearctTask() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+export default function SearchTask({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleClick(event) {
+    event.preventDefault();
+    onSearch(searchTerm);
+  }
+
   return (
     <form>
       <div className="flex">
@@ -6,6 +15,8 @@ export default function SearctTask() {
           <input
             type="search"
             id="search-dropdown"
+            value={searchTerm}
+            onChange={() => setSearchTerm(event.target.value)}
             className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none"
             placeholder="Search Task"
             required
@@ -13,6 +24,7 @@ export default function SearctTask() {
           <button
             type="submit"
             className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
+            onClick={handleClick}
           >
             <svg
               className="h-4 w-4"
--- src/TaskManager/TaskBoard.jsx
+++ src/TaskManager/TaskBoard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTaskModal from "./AddTaskModal";
-import SearctTask from "./SearchTask";
+import NoTaskFound from "./NoTaskFound";
+import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
@@ -56,6 +57,23 @@ export default function TaskBoard() {
     setTasks([...tasks]);
   }
 
+  function handleFavorite(tasKId) {
+    const taskIndex = tasks.findIndex(task => task.id === tasKId);
+
+    const newTasks = [...tasks];
+
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
+  }
+
+  function handleSearch(searchTerm) {
+    const filtered = tasks.filter(task =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setTasks([...filtered]);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddModal && (
@@ -67,18 +85,23 @@ export default function TaskBoard() {
       )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearctTask />
+          <SearchTask onSearch={handleSearch} />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction
             onAddClick={() => setShowAddModal(true)}
             onDeleteAllClick={onDeleteAllClick}
           />
-          <TaskList
-            tasks={tasks}
-            onEdit={handleEditTask}
-            onDelete={handeleDeleteTask}
-          />
+          {tasks.length > 0 ? (
+            <TaskList
+              tasks={tasks}
+              onEdit={handleEditTask}
+              onDelete={handeleDeleteTask}
+              onFav={handleFavorite}
+            />
+          ) : (
+            <NoTaskFound />
+          )}
         </div>
       </div>
     </section>
